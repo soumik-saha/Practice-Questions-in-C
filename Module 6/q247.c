2887353102f14e75e2b78819b4349fac91a27a7b
SOUMIK SAHA <103322943+soumik-saha@users.noreply.github.com>
2022-06-26 01:10:34 +0530

    Add files via upload

---
+++ Module 6/q247.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[50];
+    printf("Enter a string : ");
+    scanf("%s",str[50]);
+    printf("%s",)
+    int length=strlen(str);
+    // printf("The length of the %s is %d",str[50],length);
+    return 0;
+}
